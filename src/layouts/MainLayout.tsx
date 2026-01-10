3f2459746f3928a43e0778d1159fb5fce1fa2427
intankirana19 <intankirana19@gmail.com>
2026-01-10 17:49:31 +0700

    add useToggle custom hook

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,29 +1,29 @@
-import { useState } from "react"
+// src/layouts/MainLayout.tsx
 import { Outlet } from "react-router-dom"
 import Header from "../components/Header"
 import Sidebar from "../components/Sidebar"
+import { useToggle } from "../hooks/useToggle"
 
 export default function MainLayout() {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
+  const sidebar = useToggle(false)
 
   return (
     <div className="min-h-screen bg-gray-100">
-        <Header onToggleSidebar={() => setSidebarOpen((v) => !v)} />
+      <Header onToggleSidebar={sidebar.toggle} />
 
-        <Sidebar
-            open={sidebarOpen}
-            onClose={() => setSidebarOpen(false)}
-            onToggle={() => setSidebarOpen((v) => !v)}
-        />
+      <Sidebar
+        open={sidebar.value}
+        onClose={sidebar.close}
+        onToggle={sidebar.toggle}
+      />
 
-
-        <main
-            className={`transition-all duration-200 p-6 ${
-            sidebarOpen ? "ml-56" : "ml-0"
-            }`}
-        >
-            <Outlet />
-        </main>
+      <main
+        className={`transition-all duration-200 p-6 ${
+          sidebar.value ? "ml-56" : "ml-0"
+        }`}
+      >
+        <Outlet />
+      </main>
     </div>
   )
 }
