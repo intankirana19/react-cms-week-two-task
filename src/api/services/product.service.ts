4a9accc9aeadbdd2c73ac5aadb83af4d10f18f5f
intankirana19 <intankirana19@gmail.com>
2026-01-13 16:51:19 +0700

    refer to khatulistiwa sc: create api service

---
+++ src/api/services/product.service.ts
@@ -0,0 +1,12 @@
+// import { PRODUCT_API } from "../../constants/constants";
+import { apiClient } from "../../lib/axios";
+import { PRODUCT_ROUTES } from "../constants";
+import type { Product } from "../schemas/product.schema";
+
+export const productService = {
+    getList: async (): Promise<Product[]> => {
+        const url = `${PRODUCT_ROUTES.BASE}` // blm ada qparam
+        const response = await apiClient.get<Product[]>(url)
+        return response.data
+    }
+}
