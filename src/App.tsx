3c1d80518f44b55ec447210dd271d5391f322037
intankirana19 <intankirana19@gmail.com>
2026-01-14 16:31:57 +0700

    react-error-boundary lib trial as error boundaries

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
-import AppRouter from "./app/router"
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./app/router";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ErrorBoundary } from "react-error-boundary";
+import type { ErrorInfo } from "react";
 
-export default function App() {
-  return <AppRouter />
+function logErrorToService(error: Error, info: ErrorInfo) {
+  console.error("Logging error:", error, info);
+}
+
+export function App() {
+  return (
+    <ErrorBoundary 
+        fallbackRender={({ error, resetErrorBoundary }) => (
+            <div role="alert">
+              <p>Eror: {error.message}</p>
+              <button onClick={resetErrorBoundary}>Ulang</button>
+            </div>
+          )} 
+        onError={logErrorToService}
+    >
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <AppRouter />
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ErrorBoundary>
+  );
 }
