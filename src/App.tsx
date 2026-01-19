e14b0d065e860d8c8643027a8a93120f319d636f
intankirana19 <intankirana19@gmail.com>
2026-01-19 15:55:33 +0700

    integrate react query to error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider, QueryErrorResetBoundary } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./app/router";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { ErrorBoundary } from "react-error-boundary";
+import { ErrorBoundary, type FallbackProps } from "react-error-boundary";
 import type { ErrorInfo } from "react";
+
+function ErrorFallback({ error, resetErrorBoundary }: Readonly<FallbackProps>) {
+  return (
+    <div role="alert" className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white flex flex-col items-center justify-center p-6 rounded shadow min-w-[20rem] w-auto space-y-4">
+        <h1 className="text-5xl font-bold text-center text-[#7B1E3A]">
+          Error
+        </h1>
+        <p className="text-xl text-center">
+          {error.message}
+        </p>
+        <button
+          className="bg-[#7B1E3A] text-white text-center w-full p-2 rounded hover:bg-[#9B2C4A] cursor-pointer" 
+          onClick={resetErrorBoundary}>
+            Ulang
+        </button>
+      </div>
+    </div>
+  );
+}
 
 function logErrorToService(error: Error, info: ErrorInfo) {
   console.error("Logging error:", error, info);
@@ -12,21 +32,21 @@ function logErrorToService(error: Error, info: ErrorInfo) {
 
 export function App() {
   return (
-    <ErrorBoundary 
-        fallbackRender={({ error, resetErrorBoundary }) => (
-            <div role="alert">
-              <p>Eror: {error.message}</p>
-              <button onClick={resetErrorBoundary}>Ulang</button>
-            </div>
-          )} 
-        onError={logErrorToService}
-    >
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <AppRouter />
-        </BrowserRouter>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </ErrorBoundary>
+    <QueryErrorResetBoundary>
+      {({ reset }) => (
+        <ErrorBoundary
+          onReset={reset}
+          FallbackComponent={ErrorFallback}
+          onError={logErrorToService}
+        >
+              <QueryClientProvider client={queryClient}>
+                <BrowserRouter>
+                  <AppRouter />
+                </BrowserRouter>
+                <ReactQueryDevtools initialIsOpen={false} />
+              </QueryClientProvider>
+        </ErrorBoundary>
+      )}
+    </QueryErrorResetBoundary>
   );
 }
