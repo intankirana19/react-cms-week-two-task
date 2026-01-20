f1134c48646f7e0b1eea2beccae67e68f953c0b6
intankirana19 <intankirana19@gmail.com>
2026-01-20 16:05:25 +0700

    apply removal confirm dialog replacing remove button with decreasing amount less than 1 validation

--- src/components/CartItemRow.tsx
+++ src/components/CartItemRow.tsx
@@ -1,27 +1,43 @@
-import { memo, useCallback } from "react"
+import { memo, useCallback, useState } from "react"
 import type { CartItem } from "../types/cart"
 import { useCartStore } from "../store/cart.store"
+import { ConfirmRemoveDialog } from "./ConfirmRemoveDialog"
 
 export const CartItemRow = memo(function CartItemRow({ item }: { item: CartItem }) {
   const updateQty = useCartStore((s) => s.updateQty)
   const removeItem = useCartStore((s) => s.removeItem)
 
+  const [confirmOpen, setConfirmOpen] = useState(false)
+
   const inc = useCallback(() => updateQty(item.id, item.quantity + 1), [item, updateQty])
-  const dec = useCallback(() => updateQty(item.id, item.quantity - 1), [item, updateQty])
+  const dec = useCallback(() => {
+      if (item.quantity === 1) setConfirmOpen(true)
+      else updateQty(item.id, item.quantity - 1)
+  }, [item, updateQty])
 
   return (
-    <div className="flex items-center justify-between border-b pb-2">
-      <div>
-        <p className="font-medium">{item.name}</p>
-        <p className="text-sm">{item.price}</p>
-      </div>
+    <>
+      <div className="flex items-center justify-between border-b pb-2">
+        <div>
+          <p className="font-medium">{item.name}</p>
+          <p className="text-sm">{item.price}</p>
+        </div>
 
-      <div className="flex items-center gap-2">
-        <button onClick={dec}>-</button>
-        <span>{item.quantity}</span>
-        <button onClick={inc}>+</button>
-        <button onClick={() => removeItem(item.id)}>Hapus</button>
+        <div className="flex items-center gap-2">
+          <button onClick={dec} className="border px-2 rounded">-</button>
+          <span>{item.quantity}</span>
+          <button onClick={inc} className="border px-2 rounded">+</button>
+          {/* <button onClick={() => removeItem(item.id)}>Hapus</button> */}
+        </div>
       </div>
-    </div>
+      <ConfirmRemoveDialog
+              open={confirmOpen}
+              onClose={() => setConfirmOpen(false)}
+              onConfirm={() => {
+                removeItem(item.id)
+                setConfirmOpen(false)
+              }}
+            />
+    </>
   )
 })
