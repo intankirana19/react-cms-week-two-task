9a57fa65ab70508d66942cf3cb4544295a34b7c5
intankirana19 <intankirana19@gmail.com>
2026-01-22 16:07:31 +0700

    changes adjustmen

--- src/api/services/product.service.ts
+++ src/api/services/product.service.ts
@@ -1,12 +1,12 @@
 // import { PRODUCT_API } from "../../constants/constants";
 import { apiClient } from "../../shared/lib/axios";
 import { PRODUCT_ROUTES } from "../constants/routes";
-import type { Product } from "../schemas/product.schema";
+import type { ProductSchemaType } from "../schemas/product.schema";
 
 export const productService = {
-    getList: async (): Promise<Product[]> => {
+    getList: async (): Promise<ProductSchemaType[]> => {
         const url = `${PRODUCT_ROUTES.BASE}` // blm ada qparam
-        const response = await apiClient.get<Product[]>(url)
+        const response = await apiClient.get<ProductSchemaType[]>(url)
         return response.data
     }
 }
