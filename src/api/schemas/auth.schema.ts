fd74a8512bc2c7bb06653e14fee71aacaffd4b26
intankirana19 <intankirana19@gmail.com>
2026-01-28 16:34:42 +0700

    adjust auth schema

--- src/api/schemas/auth.schema.ts
+++ src/api/schemas/auth.schema.ts
@@ -1,13 +1,14 @@
 import z from "zod";
 
 export const authInputSchema = z.object({
-    email: z
-            .string()
-            .min(1, "Email wajib diisi")
-            .refine(
-                (val) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val),
-                { message: "Format email salah" }
-            ),
+    // email: z
+    //         .string()
+    //         .min(1, "Email wajib diisi")
+    //         .refine(
+    //             (val) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val),
+    //             { message: "Format email salah" }
+    //         ),
+    username: z.string().min(1, "Username wajib diisi"),
     password: z.string().min(1, "Password wajib diisi").min(8, "Password minimal 8 karakter"),
 });
 export type AuthInputSchemaType = z.infer<typeof authInputSchema>
