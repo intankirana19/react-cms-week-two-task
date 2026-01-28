774edeadca5e7dfe6fddb3944308c5d1471029cc
intankirana19 <intankirana19@gmail.com>
2026-01-28 16:35:02 +0700

    adjust auth store into storing user data (mocked)

--- src/features/auth/stores/auth.store.ts
+++ src/features/auth/stores/auth.store.ts
@@ -1,17 +1,20 @@
 import { create } from "zustand"
 import { createJSONStorage, persist } from "zustand/middleware"
+import type { UserSchemaType } from "../../../api/schemas/user.schema"
 
 type AuthState = {
+  user: UserSchemaType | null
   isAuthenticated: boolean
-  login: () => void
+  login: (user: UserSchemaType | null) => void
   logout: () => void
 }
 
 export const useAuthStore = create<AuthState>()(
   persist( // pakai persist utk simpan ke localstorage,kalau udh ada api?
     (set) => ({
+      user: null,
       isAuthenticated: false,
-      login: () => set({ isAuthenticated: true }),
+      login: (user) => set({ user,isAuthenticated: true }),
       logout: () => set({ isAuthenticated: false }),
     }),
     {
