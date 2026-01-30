60852fca351deade7501c0b8d67ca5413fd19e8c
intankirana19 <intankirana19@gmail.com>
2026-01-30 10:59:34 +0700

    create login & logout service (apis mock)

--- src/api/services/auth.service.ts
+++ src/api/services/auth.service.ts
@@ -2,11 +2,28 @@ import { userAdminMock, userBuyerMock } from "../mocks/user"
 import type { AuthInputSchemaType } from "../schemas/auth.schema"
 import type { UserSchemaType } from "../schemas/user.schema"
 
-export const productService = {
+export const authService = {
     login: async (data: AuthInputSchemaType): Promise<UserSchemaType> => {
         // const url = `${AUTH_ROUTES.BASE}` // blm ada API
         // const response = await apiClient.get<UserSchemaType[]>(url) // blm ada API
-        const response = data.username === 'admin1' ? userAdminMock : userBuyerMock
-        return response
+        if (data.username === 'admin1' || data.username === 'buyer') { 
+            if (data.username === 'admin1') { 
+                return userAdminMock 
+            } else {
+                return userBuyerMock
+            }
+        } else {
+            throw new Error('Invalid username or password')
+        } 
+    },
+
+    logout: async () => {
+        // await apiClient.post(AUTH_ROUTES.LOGOUT); // blm ada api
+        await new Promise((res) => setTimeout(res, 300));
+
+        return {
+            data: { success: true },
+            status: 200
+        };
     }
 }
