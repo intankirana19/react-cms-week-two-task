66655e0e50d3b99e313b09ae6427abadc3425221
intankirana19 <intankirana19@gmail.com>
2026-01-30 11:00:32 +0700

    refactor auth store for mutation

--- src/features/auth/hooks/useAuth.ts
+++ src/features/auth/hooks/useAuth.ts
@@ -1,6 +1,6 @@
 import { useAuthStore } from "../stores/auth.store"
 
 export function useAuth() {
-  const { isAuthenticated, login, logout } = useAuthStore()
+  const { isAuthenticated, login, logout } = useAuthStore() //store udh diganti buat mutation useLogin
   return { isAuthenticated, login, logout }
 }
--- src/features/auth/stores/auth.store.ts
+++ src/features/auth/stores/auth.store.ts
@@ -3,19 +3,39 @@ import { createJSONStorage, persist } from "zustand/middleware"
 import type { UserSchemaType } from "../../../api/schemas/user.schema"
 
 type AuthState = {
+  accessToken: string | null;
   user: UserSchemaType | null
-  isAuthenticated: boolean
-  login: (user: UserSchemaType | null) => void
-  logout: () => void
+  isAuthenticated: boolean;
+  setAccessToken: (token: string | null) => void;
+  login: (token: string, user: UserSchemaType) => void;
+  logout: () => void;
 }
 
 export const useAuthStore = create<AuthState>()(
   persist( // pakai persist utk simpan ke localstorage,kalau udh ada api?
-    (set) => ({
+    (set,get) => ({
+      accessToken: null,
       user: null,
       isAuthenticated: false,
-      login: (user) => set({ user,isAuthenticated: true }),
-      logout: () => set({ isAuthenticated: false }),
+      setAccessToken: (token) =>
+        set({
+          accessToken: token,
+          isAuthenticated: !!token,
+        }),
+      login: (token,user) => set({ 
+        accessToken: token,
+        user,
+        isAuthenticated: true 
+      }),
+      logout: () => set({ 
+        accessToken: null,
+        user: null,
+        isAuthenticated: false 
+      }),
+      initialize: () => {
+        const state = get();
+        set({ isAuthenticated: !!state.accessToken });
+      },
     }),
     {
       name: "auth-storage",
