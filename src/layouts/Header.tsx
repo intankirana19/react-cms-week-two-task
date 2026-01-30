6023220edae6d6b45eb557b4012517e76ab320fc
intankirana19 <intankirana19@gmail.com>
2026-01-30 16:56:48 +0700

    apply role validation to header components

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -4,12 +4,16 @@ import { HeaderCart } from "../features/cart/components/HeaderCart"
 import { Button } from "../shared/components/Button"
 import { LogOut, Menu } from "lucide-react"
 import { useLogout } from "../features/auth/hooks/useLogout"
+import { useAuthStore } from "../features/auth/stores/auth.store"
+import { role } from "../shared/constants/role"
 
 type Props = {
   onToggleSidebar: () => void
 }
 
 export default function Header({ onToggleSidebar }: Readonly<Props>) {
+  const {user} = useAuthStore();
+
   // const { logout } = useAuth()
   // const navigate = useNavigate()
   const logoutMutation = useLogout()
@@ -26,12 +30,12 @@ export default function Header({ onToggleSidebar }: Readonly<Props>) {
         <Menu onClick={onToggleSidebar} ></Menu>
 
         <span className="font-semibold tracking-wide">
-          ADMIN TOKO INTAN
+          { user?.role === role.admin ? 'ADMIN ' : ''}TOKO INTAN
         </span>
       </div>
 
       <div className="flex items-center gap-6">
-        <HeaderCart/>
+        {user?.role === role.buyer && <HeaderCart/> }
 
         <Button variant="tertiary" size="sm" className="flex gap-2" onClick={handleLogout}>
           <LogOut></LogOut>
